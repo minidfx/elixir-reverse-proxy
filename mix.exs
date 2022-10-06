dbda19296a9694fcba869acdeda9c5ad000e9243
Burgy Benjamin <benjamin@roomz.io>
2022-10-06 22:25:54 +0200

    Bump version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Couloir42ReverseProxy.MixProject do
   def project do
     [
       app: :couloir42_reverse_proxy,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
