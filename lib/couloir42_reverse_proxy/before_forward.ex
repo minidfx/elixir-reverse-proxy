939e48591b38bf0e8225a16ab2be82d62ba120b1
Burgy Benjamin <benjamin@roomz.io>
2022-10-26 22:21:39 +0200

    Use behaviour for the plug before forwarding requests.

--- lib/couloir42_reverse_proxy/before_forward.ex
+++ lib/couloir42_reverse_proxy/before_forward.ex
@@ -1,11 +1,13 @@
 defmodule Couloir42ReverseProxy.BeforeForward do
-  @spec init(any) :: any
+  @behaviour Plug
+
+  @impl true
   def init(options) do
     # initialize options
     options
   end
 
-  @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
+  @impl true
   def call(conn, _opts) do
     # INFO: Hook for disabling/enabling the proxy.
     conn
